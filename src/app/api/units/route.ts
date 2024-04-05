d99406afc31c74027772e1984e32582c3a08a375
aimahusnain <aimahusnain@gmail.com>
2024-04-05 15:43:13 +0500

    Hello

--- src/app/api/units/route.ts
+++ src/app/api/units/route.ts
@@ -19,8 +19,6 @@ export async function GET(request: NextRequest) {
       });
     }
   } catch (e) {
-    console.log(e);
-
     return NextResponse.json({
       success: false,
       message: "Something went wrong ! Please try again",
