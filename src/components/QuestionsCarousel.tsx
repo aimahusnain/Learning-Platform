cddeb35104ba6a030b2d50bf324c89bf056c2c23
aimahusnain <aimahusnain@gmail.com>
2024-04-08 21:29:25 +0500

    sette

--- src/components/QuestionsCarousel.tsx
+++ src/components/QuestionsCarousel.tsx
@@ -24,7 +24,7 @@ export const QuestionsCarousel: React.FC<Props> = () => {
     const fetchQuestions = async () => {
       try {
         const response = await fetch(
-          `http://localhost:3000/api/mainquestions?id=cluok50v30000oa6trymosjad`
+          `${StaticData.SiteURL}/api/mainquestions?id=cluok50v30000oa6trymosjad`
         );
         const data = await response.json();
         if (data.success) {
