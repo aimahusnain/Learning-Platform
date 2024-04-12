e55ab04fb21a7f6a5446a0e522e0bb7f3e75b2da
aimahusnain <aimahusnain@gmail.com>
2024-04-12 19:16:25 +0500

    asdf

--- src/components/QuestionsPageCompo/Submit Button.tsx
+++ src/components/QuestionsPageCompo/Submit Button.tsx
@@ -16,15 +16,18 @@ const SubmitButton: React.FC<SubmitButtonProps> = ({ isSubmitted }) => {
 
     refresh();
     try {
-      const response = await fetch(`${StaticData.SiteURL}/api/submitQuestion`, {
-        method: "PUT",
-        headers: {
-          "content-type": "application/x-www-form-urlencoded",
-        },
-        body: JSON.stringify({
-          id: "cluok50v30000oa6trymosjad",
-        }),
-      });
+      const response = await fetch(
+        `${StaticData.SiteURL}/api/submitQuestion`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: "cluok50v30000oa6trymosjad",
+          }),
+        }
+      );
 
       const data = await response.json();
 
