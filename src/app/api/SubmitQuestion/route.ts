b6dfc3766ecd39c3a783c7893adab3a4ebe0f3b5
aimahusnain <aimahusnain@gmail.com>
2024-04-12 20:05:58 +0500

    436dfs

--- src/app/api/SubmitQuestion/route.ts
+++ src/app/api/SubmitQuestion/route.ts
@@ -1,29 +1,63 @@
+// import { db } from "@/lib/db";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function PUT(request: NextRequest) {
+//   try {
+//     const extractData = await request.json();
+
+//     const updatedBlogPost = await db.question.update({
+//       where: { id: String(extractData.id) },
+//       data: { Submitted: true },
+//     });
+
+//     if (updatedBlogPost) {
+//       return NextResponse.json({
+//         success: true,
+//         message: "Blog post updated",
+//       });
+//     } else {
+//       return NextResponse.json({
+//         success: false,
+//         message: "Failed to update the post! Please try again",
+//       });
+//     }
+//   } catch (e) {
+//     console.error(e);
+
+//     return NextResponse.json({
+//       success: false,
+//       message: "Something went wrong! Please try again",
+//     });
+//   }
+// }
+
+
+export const dynamic = "force-dynamic";
+
 import { db } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(request: NextRequest) {
   try {
-    const extractData = await request.json();
+        const extractData = await request.json();
 
-    const updatedBlogPost = await db.question.update({
-      where: { id: String(extractData.id) },
-      data: { Submitted: true },
-    });
+        const updatequestion = await db.question.update({
+          where: { id: String(extractData.id) },
+          data: { Submitted: true },
+        });
 
-    if (updatedBlogPost) {
+    if (updatequestion) {
       return NextResponse.json({
         success: true,
-        message: "Blog post updated",
+        data: updatequestion,
       });
     } else {
       return NextResponse.json({
         success: false,
-        message: "Failed to update the post! Please try again",
+        message: "No units found",
       });
     }
   } catch (e) {
-    console.error(e);
-
     return NextResponse.json({
       success: false,
       message: "Something went wrong! Please try again",
