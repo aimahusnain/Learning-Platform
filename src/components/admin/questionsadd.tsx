ef210be953c1a32308db7db28f3b282e7d0c7e04
aimahusnain <aimahusnain@gmail.com>
2024-05-09 19:57:18 +0500

    ' - &apos;

--- src/components/admin/questionsadd.tsx
+++ src/components/admin/questionsadd.tsx
@@ -95,7 +95,7 @@ const QuestionsAdd = () => {
         <DialogHeader>
           <DialogTitle>Add a Question</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes to your profile here. Click save when you&apos;re done.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
