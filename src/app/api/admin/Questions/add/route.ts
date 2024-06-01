2e5890f9e30bc2019cecd966d4d877f268ee5267
aimahusnain <aimahusnain@gmail.com>
2024-06-01 21:05:52 +0500

    GET /api/GETUserProgressMainQuestion?email=undefin

--- src/app/api/admin/Questions/add/route.ts
+++ src/app/api/admin/Questions/add/route.ts
@@ -9,7 +9,6 @@ export async function POST(request: NextRequest) {
       data: {
         name: extractPostData.name,
         description: extractPostData.description,
-        videoReferenceVideo: extractPostData.videoReferenceVideo,
         Unit: { connect: { id: extractPostData.unitId } },
       },
     });
