7e5338aaf15dd4537b86d2448fc42f3858495685
Muhammad Husnain <aimahusnain@gmail.com>
2024-07-20 09:45:46 +0500

    Change siteUrl

--- src/lib/staticdata.ts
+++ src/lib/staticdata.ts
@@ -1,6 +1,6 @@
 export const StaticData = {
   SiteName: "WordWise",
   Description: "Let's dive into English Language",
-  // SiteURL: "https://bluelan.vercel.app",
-  SiteURL: "http://localhost:3000",
+  SiteURL: "https://bluelan.vercel.app",
+  // SiteURL: "http://localhost:3000",
 };
